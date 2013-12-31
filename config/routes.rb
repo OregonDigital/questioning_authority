28d3fd75b42f5348161437ae1b770b7441b9e842
Jeremy Echols <jechols@uoregon.edu>
2013-12-31 15:16:14 -0800

    Simplify routes
    Adds optional parameters and naming for easier URLs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Qa::Engine.routes.draw do
-  match "/search/:vocab" => "terms#search", :via=>:get
-  match "/search/:vocab/:sub_authority" => "terms#search", :via=>:get
-  match "/terms/:vocab" => "terms#index", :via=>:get
-  match "/terms/:vocab/:sub_authority" => "terms#index", :via=>:get
-  match "/terms" => "terms#index", :via=>:get
+  get "/search/:vocab(/:sub_authority)", to: "terms#search", as: :search
+  get "/terms(/:vocab(/:sub_authority))", to: "terms#index", as: :terms
   match "/show/:vocab/:id" => "terms#show", :via=>:get
   match "/show/:vocab/:sub_authority/:id" => "terms#show", :via=>:get
 end
